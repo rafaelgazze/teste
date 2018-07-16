4292d30d4a37cda7b8abde47a1ded5f6fb4e0e40
rafael salem <rafael.gazze@nextel.com.br>
2018-07-16 12:54:56 -0300

    fix rules

--- app/rules/ruleblacklistmerchant.py
+++ app/rules/ruleblacklistmerchant.py
@@ -1,5 +1,6 @@
 from app.rules.ruleviolation import RuleViolation
 
-class MerchantBlackList(self, account, transaction):
-    if account.blacklist.contains(transaction.merchant):
-        return RuleViolation("Merchant Blacklisted")
+class MerchantBlackList(RuleViolation):
+    def check(self, transaction, account):
+        if transaction.merchant in account.blacklist:
+            return RuleViolation("Merchant Blacklisted")
